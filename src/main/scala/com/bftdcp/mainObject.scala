202c2e5905bd4445f3eb4977f659b66662fbeb9f
Michael Leichtfried <m.leichtfried@student.tudelft.nl>
2020-03-09 14:09:54 +0100

    Add initial definition of messages

--- src/main/scala/com/bftdcp/mainObject.scala
+++ src/main/scala/com/bftdcp/mainObject.scala
@@ -7,6 +7,48 @@ import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import Father.Command
 
+object Messages {
+  /****** Patrick ******/
+  //Initiator to Coordinator and Participant
+  final case class initCommit()
+  final case class initAbort()
+
+  //Coordinator to Participant
+  final case class prepare()
+  final case class abort()
+  final case class commit()
+
+  /****** Miguel ******/
+  //Coordinator to Initiator
+  final case class commitSuccess()
+  final case class abortSuccess()
+
+  //Participant to Initiator
+  final case class registerWithInitiator()//(page 39, start of second column seems to imply this is needed .Could perhaps be merged with the ack to initiateCommit / initiateAbort )
+
+  //Coordinator to Coordinator
+  final case class initViewChange()//(optional)
+  /****** Douwe ******/
+  final case class newView()//(optional)
+  final case class baPrepare()
+  final case class baCommit()
+
+  //Coordinator(primary) to Coordinator
+  final case class baPrePrepare()
+  final case class sendUnknownParticipants()//(see page 41, second whole paragraph)
+
+  /****** Michael ******/
+  //Coordinator to Coordinator(primary)
+  final case class requestUnknownParticipants()
+
+  //Participant to Coordinator
+  final case class registerWithCoordinator()
+  final case class prepared()
+  final case class readOnly()
+  final case class aborted()
+  final case class committed()
+}
+
 object Coordinator {
   final case class PrepareRequest(whom: String) extends Command
 
