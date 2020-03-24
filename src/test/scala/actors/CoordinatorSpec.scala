59989c7d463960564711b0e40568eb38e8e7a73f
Michael Leichtfried <m.leichtfried@student.tudelft.nl>
2020-03-24 19:51:42 +0100

    Add workaround for tests to have unique actor names

--- src/test/scala/actors/CoordinatorSpec.scala
+++ src/test/scala/actors/CoordinatorSpec.scala
@@ -6,8 +6,10 @@ import util.Messages
 import util.Messages._
 
 class CoordinatorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+  var testNr = 0
   "A transaction" must {
     "succeed with 1 coordinator and 1 participant" in {
+      testNr = testNr + 1
       val (cs, ps) = spawnAll(1, 1)
       val t = Transaction(0)
       ps.foreach(p => p ! PropagateTransaction(t))
@@ -20,6 +22,7 @@ class CoordinatorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       ps.foreach(x => testKit.stop(x))
     }
     "succeed with 4 coordinators and 1 participant" in {
+      testNr = testNr + 1
       val (cs, ps) = spawnAll(4, 1)
       val t = Transaction(0)
       ps.foreach(p => p ! PropagateTransaction(t))
@@ -32,6 +35,7 @@ class CoordinatorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       ps.foreach(x => testKit.stop(x))
     }
     "succeed with 1 coordinator and 4 participants" in {
+      testNr = testNr + 1
       val (cs, ps) = spawnAll(1, 4)
       val t = Transaction(0)
       ps.foreach(p => p ! PropagateTransaction(t))
@@ -44,6 +48,7 @@ class CoordinatorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       ps.foreach(x => testKit.stop(x))
     }
     "abort with 1 coordinator and 1 participant (participant should never be asked to prepare)" in {
+      testNr = testNr + 1
       val (cs, ps) = spawnAll(1, 1)
       val t = Transaction(0)
       ps.foreach(p => p ! PropagateTransaction(t))
@@ -56,6 +61,7 @@ class CoordinatorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       ps.foreach(x => testKit.stop(x))
     }
     "abort with 4 coordinators and 1 participant (participant should never be asked to prepare)" in {
+      testNr = testNr + 1
       val (cs, ps) = spawnAll(4, 1)
       val t = Transaction(0)
       ps.foreach(p => p ! PropagateTransaction(t))
@@ -68,6 +74,7 @@ class CoordinatorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       ps.foreach(x => testKit.stop(x))
     }
     "abort with 1 coordinator and 4 participants (participant should never be asked to prepare)" in {
+      testNr = testNr + 1
       val (cs, ps) = spawnAll(1, 4)
       val t = Transaction(0)
       ps.foreach(p => p ! PropagateTransaction(t))
@@ -80,18 +87,20 @@ class CoordinatorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       ps.foreach(x => testKit.stop(x))
     }
     "be able to abort a transaction" in {
+      testNr = testNr + 1
       val (cs, ps) = spawnAll(1, 0, 1)
       val t = Transaction(0)
       ps.foreach(p => p ! PropagateTransaction(t))
       Thread.sleep(100) // make sure the transaction fully propagated
       val p = ps(0)
       LoggingTestKit.info("Aborted transaction 0").withOccurrences(1).expect {
-        cs.foreach(c => c ! Messages.InitCommit(t.id,  p))
+        cs.foreach(c => c ! Messages.InitCommit(t.id, p))
       }
       cs.foreach(x => testKit.stop(x))
       ps.foreach(x => testKit.stop(x))
     }
     "be able to unilaterally abort a transaction (1 coordinator)" in {
+      testNr = testNr + 1
       val (cs, ps) = spawnAll(1, 4, 1)
       val t = Transaction(0)
       ps.foreach(p => p ! PropagateTransaction(t))
@@ -104,6 +113,7 @@ class CoordinatorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       ps.foreach(x => testKit.stop(x))
     }
     "be able to unilaterally abort a transaction (4 coordinators)" in {
+      testNr = testNr + 1
       val (cs, ps) = spawnAll(4, 4, 1)
       val t = Transaction(0)
       ps.foreach(p => p ! PropagateTransaction(t))
@@ -118,6 +128,7 @@ class CoordinatorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
   }
   "2 transactions" must {
     "succeed" in {
+      testNr = testNr + 1
       val (cs, ps) = spawnAll(1, 1)
       val t0 = Transaction(0)
       ps.foreach(p => p ! PropagateTransaction(t0))
@@ -139,15 +150,15 @@ class CoordinatorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
   def spawnAll(nCoordinators: Int, nCommittingParticipants: Int, nAbortingParticipants: Int = 0): (Array[Messages.Coordinator], Array[Messages.Participant]) = {
     val cs = new Array[Messages.Coordinator](nCoordinators)
     for (x <- 0 until nCoordinators) {
-      cs(x) = spawn(Coordinator(), "Coordinator-" + x)
+      cs(x) = spawn(Coordinator(), testNr + "Coordinator-" + x)
     }
     cs.foreach { x => x ! Messages.Setup(cs) }
     val ps = new Array[Messages.Participant](nCommittingParticipants + nAbortingParticipants)
     for (x <- 0 until nCommittingParticipants) {
-      ps(x) = spawn(Participant(cs, Decision.COMMIT), "Participant-" + x)
+      ps(x) = spawn(Participant(cs, Decision.COMMIT), testNr + "Participant-" + x)
     }
     for (x <- nCommittingParticipants + 1 until nCommittingParticipants + nAbortingParticipants) {
-      ps(x) = spawn(Participant(cs, Decision.ABORT), "Participant-" + x)
+      ps(x) = spawn(Participant(cs, Decision.ABORT), testNr + "Participant-" + x)
     }
     (cs, ps)
   }
