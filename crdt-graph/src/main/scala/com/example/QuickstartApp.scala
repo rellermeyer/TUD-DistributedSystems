09af8426a86bb8c5a4fe0a74a376351fd653108c
Erwin <Erwin@erwinrussel.com>
2021-03-13 15:02:56 +0100

    remove unused import

--- crdt-graph/src/main/scala/com/example/QuickstartApp.scala
+++ crdt-graph/src/main/scala/com/example/QuickstartApp.scala
@@ -7,7 +7,6 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
-import com.typesafe.config.{Config, ConfigFactory, ConfigValueFactory}
 
 import scala.io.StdIn
 
