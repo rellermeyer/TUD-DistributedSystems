eba8b7597247159f29d505c9d5f538917b291dad
Jaap Blok <j.h.blok-1@student.tudelft.nl>
2021-03-25 15:05:47 +0100

    add experiment instance

--- Weighted_Voting_for_Replicated_Data/src/main/scala/Experiment.scala
+++ Weighted_Voting_for_Replicated_Data/src/main/scala/Experiment.scala
@@ -10,16 +10,15 @@ import scala.jdk.CollectionConverters._
 
 object Experiment {
 
-  val readProportion: Double = 0.5
+  val readProportions: Seq[Double] = Seq(1.0)
   val numCalls: Int = 10000
 
   val numContainers: Int = 10
   val latencies: Seq[Int] = Seq(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-  val failureProbs: Seq[Double] = Seq(0.0, 0.1, 0.2, 0.3, 0.4, 0.5)
+  val failureProbs: Seq[Double] = Seq(0.00, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50)
 
   val suiteId: Int = 1
-  val suiteR: Int = 6
-  val suiteW: Int = 6
+  val suiteRWPairs: Seq[(Int, Int)] = Seq((6, 6), (7, 7))
   val repWeights: Seq[Int] = Seq(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 
   def main(args: Array[String]): Unit = {
@@ -30,13 +29,13 @@ object Experiment {
     val file = new BufferedWriter(new FileWriter("C:\\Users\\blok_\\Desktop\\Results.csv"))
     val csvWriter = new CSVWriter(file)
     val records = new ListBuffer[Array[String]]()
-    val csvColumns = Array("container failure probability", "successful calls", "failed calls", "average latency")
+    val csvColumns = Array("container failure probability", "read proportion", "r", "w", "successful calls", "failed calls", "average latency")
     records += csvColumns
 
-    for (p <- failureProbs) {
-      val fileSystem = DistributedSystem(numContainers, latencies, p)
+    for (fProb <- failureProbs; readProp <- readProportions; rw <- suiteRWPairs) {
+      val fileSystem = DistributedSystem(numContainers, latencies, fProb)
       val manager = FileSuiteManager(fileSystem)
-      fileSystem.createRepresentatives(suiteId, suiteR, suiteW, repWeights)
+      fileSystem.createRepresentatives(suiteId, rw._1, rw._2, repWeights)
       var successCount: Int = 0
       var failCount: Int = 0
       var totalLatency: Int = 0
@@ -44,10 +43,13 @@ object Experiment {
       var fileContent: Int = -1
 
       for (i <- 0 until numCalls) {
-        if (event >= readProportion) {
+        if (event <= readProp) {
           val result = manager.read(suiteId)
           result match {
-            case Left(f) => failCount = failCount + 1
+            case Left(f) => {
+              failCount = failCount + 1
+              //print(f.reason)
+            }
             case Right(r) => {
               successCount = successCount + 1
               totalLatency = totalLatency + r._2
@@ -57,7 +59,10 @@ object Experiment {
         else {
           val result = manager.write(suiteId, 1)
           result match {
-            case Left(f) => failCount = failCount + 1
+            case Left(f) => {
+              failCount = failCount + 1
+              //print(f.reason)
+            }
             case Right(r) => {
               successCount = successCount + 1
               totalLatency = totalLatency + r
@@ -67,8 +72,10 @@ object Experiment {
         event = r.nextDouble()
       }
       val avgLatency: Double = totalLatency.toDouble/successCount.toDouble
-      println("p: " + p + ", successes: " + successCount + ", failures: " + failCount + ", avg latency: " + avgLatency)
-      records += Array(p.toString, successCount.toString , failCount.toString , avgLatency.toString)
+      println("failure probability: " + fProb + ", read proportion: " + readProp + ", r: " + rw._1 + ", w: " + rw._2 +
+        ", successes: " + successCount + ", failures: " + failCount + ", avg latency: " + avgLatency)
+      records += Array(fProb.toString, readProp.toString, rw._1.toString, rw._2.toString,
+        successCount.toString , failCount.toString , avgLatency.toString)
     }
     try {
       csvWriter.writeAll(records.toList.asJava)
